6250abf1d5bcdff04b7a4178fbe62783050d923c
vladislavis <vladislavis@is.com>
2021-09-02 17:25:46 +0500

    error

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
-import "fmt"
+import ("fmt"; "errors")
 
 const pi = 3.14
 
 func main(){
 	var pharse = "Go рулит!"
 	fmt.Println(pharse);
+	printCircleArea(0);
 	printCircleArea(2);
 	printCircleArea(4);
 
@@ -14,14 +15,25 @@ func main(){
 }
 
 func printCircleArea(radius int) {
+
+	circleArea, err := calculateCircleArea(radius);
+
+	if err != nil {
+		fmt.Println(err.Error());
+		return
+	}
+
+	
 	fmt.Printf("Радиус круга: %d cm.\n", radius);
 	fmt.Printf("Формула для расчета площади круга A=nr2\n");
 
-	circleRadius := calculateCircleArea(radius);
-
-	fmt.Printf("Площадь круга: %f32 см. кв.\n", circleRadius);
+	fmt.Printf("Площадь круга: %f32 см. кв.\n", circleArea);
 }
 
-func calculateCircleArea(radius int) float32 {
-	return float32(radius) * float32(radius) * pi
+func calculateCircleArea(radius int) (float32, error) {
+	if radius <= 0 {
+		fmt.Println("Радиус круга не может быть меньше 0");
+		return float32(0), errors.New("Радиус круга не может быть отрицательным");
+	}
+	return float32(radius) * float32(radius) * pi, nil
 }
