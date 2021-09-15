50c5d0776954fa75b16d42332ee37423b75dc762
Кудряков Владислав <vladislavis@intersvyaz.net>
2021-09-15 23:15:12 +0500

    фыв

--- goroutines.go
+++ goroutines.go
@@ -14,10 +14,19 @@ func main(){
 	//Горутины это по сути потоки в go
 	fmt.Println("Сравните время исполнения");
 
-	fmt.Println("Время исполнения без горутинов");
+	//fmt.Println("Время исполнения без горутинов");
 	//Очень сложные вычисления
-	calculateSomething(1000);
-	fmt.Println("Время исполнения с горутинами");
+	go calculateSomething(1000)
+	//fmt.Println("Время исполнения с горутинами");
+
+	//Использование корутин через слово go
+	go calculateSomething(2000)
+
+	//блокирует выполнение главной горутины,
+	//планировщик передает контекст на другие незаблокированные горутины
+	time.Sleep(8 * time.Second)
+	fmt.Printf("Время выполнения программы: %s\n", time.Since(t))
+
 
 
 
@@ -28,11 +37,24 @@ func calculateSomething(n int){
 	t := time.Now()
 
 	result := 0
-	for i:=-; i <= n; i++ {
+	for i:=0; i <= n; i++ {
 		result += i * 2
 		time.Sleep(time.Millisecond * 3)
 	}
 
-	fmt.Printf("Старт: %s\n", t.Format(time.RFC3339))
+	fmt.Printf("Результат: %d; Прошло времени %s\n", result, time.Since(t))
 }
+
+func calculateSomething1(n int){
+	t := time.Now()
+
+	result := 0
+	for i:=0; i <= n; i++ {
+		result += i * 2
+		time.Sleep(time.Millisecond * 3)
+	}
+
+	fmt.Printf("Результат2: %d; Прошло времени %s\n", result, time.Since(t))
+}
+
 
