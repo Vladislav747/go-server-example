02878937535c5e79fa3f63eb693b1870c2d7df80
vladislavis <vladislavis@is.com>
2021-11-01 16:51:41 +0500

    fix

--- buf.go
+++ buf.go
@@ -10,16 +10,23 @@ func main() {
 	var phrase = "Go Buf"
 	fmt.Println(phrase)
 
-	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		counts[input.Text()]++
-	}
+	//counts := make(map[string]int)
+	input := bufio.NewReader(os.Stdin)
 
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
-	}
+	
+	for {
+		line, _ := input.ReadString('\n')
+		fmt.Print(line)
+  	}
+
+	// for input.Scan() {
+	// 	counts[input.Text()]++
+	// }
+
+	// for line, n := range counts {
+	// 	if n > 1 {
+	// 		fmt.Printf("%d\t%s\n", n, line)
+	// 	}
+	// }
 
 }
