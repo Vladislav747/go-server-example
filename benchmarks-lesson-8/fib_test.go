8181e5b0607a02b896a3b3f4a8dd06a111b88c46
vladislavis <vladislavis@intersvyaz.net>
2022-03-08 23:55:22 +0500

    Updated benchmarks-lesson-8/fib_test.go

--- benchmarks-lesson-8/fib_test.go
+++ benchmarks-lesson-8/fib_test.go
@@ -13,11 +13,15 @@ func main() {
 
 func BenchmarkFib10(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Fib(40)
+		Fib(10)
 	}
-	//b.RunParallel(func(pb *testing.PB) {
-	//	for pb.Next() {
-	//		Fib(10)
-	//	}
-	//})
+
+}
+
+func BenchmarkFibParallel10(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			Fib(10)
+		}
+	})
 }
