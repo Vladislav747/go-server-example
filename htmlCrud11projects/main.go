5117b2b64c5066098e2edafd85065121e2d788a4
vladislavis <vladislavis@intersvyaz.net>
2022-05-02 03:41:10 +0500

    Added crud

--- htmlCrud11projects/main.go
+++ htmlCrud11projects/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
 type Movie struct {
 	ID       string    `json: "id"`
 	Isbn     string    `json: "isbn"`
@@ -14,9 +20,19 @@ type Director struct {
 
 var movies []Movie
 
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
 func main() {
 	r := mux.NewRouter()
 
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "45455", Title: "Movie Two", Director: &Director{Firstname: "Steve", Lastname: "Smith"}})
+
 	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies/", createMovie).Methods("POST")
+	r.HandleFunc("/movies/", updateMovie).Methods("PUT")
 }
