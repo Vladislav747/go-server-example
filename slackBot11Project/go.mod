a60d73e330535ffc70e5a169246f64996b81dd3b
Vladislav747 <zippo585@gmail.com>
2022-05-03 03:50:46 +0500

    Updated slackBot11projects

--- slackBot11Project/go.mod
+++ slackBot11Project/go.mod
@@ -1,3 +1,5 @@
 module github.com/slackbot
 
 go 1.15
+
+require github.com/shomali11/slacker v0.0.0-20220129203130-6c28a41fb7b0
--- slackBot11Project/main.go
+++ slackBot11Project/main.go
@@ -1,14 +1,55 @@
 package main
 
 import (
-	"github.com/showmali11/slacker"
+	"context"
+	"fmt"
+	"github.com/shomali11/slacker"
+	"log"
 	"os"
+	"strconv"
 )
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Evenets")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
 
 func main() {
 	//API Slackbot
 	os.Setenv("SLACK_BOT_TOKEN", "")
 	os.Setenv("SLACK_APP_TOKEN", "")
 
-	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"))
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	//Горутина обработки команд
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("My job is to...", &slacker.CommandDefinition{
+		Description: "you calcualtor",
+		Example:     "my job is 2020",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				println("error")
+			}
+			age := 2021 - yob
+			r := fmt.Sprintf("age is %d", age)
+			response.Reply(r)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
