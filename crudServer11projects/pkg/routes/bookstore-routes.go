ca1dcdfb839f5d7b535133d1e1c41cd429874fa3
Vladislav747 <zippo585@gmail.com>
2022-05-02 10:35:07 +0500

    Added Benchmarks

--- crudServer11projects/pkg/routes/bookstore-routes.go
+++ crudServer11projects/pkg/routes/bookstore-routes.go
@@ -1,11 +1,12 @@
 package routes
 
 import (
-	"github.com/gorilla/mux"
 	"github.com/akhil/go-bookstore/pkg/controllers"
+	"github.com/gorilla/mux"
 )
 
-var RegisterBookStoreRoutes = func(router *mux.Router){
+var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Method("POST")
 	router.HandleFunc("/book/", controllers.CreateBook).Method("GET")
+	router
 }
