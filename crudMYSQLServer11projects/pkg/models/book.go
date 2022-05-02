57a7bd68f841866215b45e2ecbfaeadaae79e6cd
Vladislav747 <zippo585@gmail.com>
2022-05-02 21:12:30 +0500

    Updated htmlCrudMysql11projects

---
+++ crudMYSQLServer11projects/pkg/models/book.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"github.com/crudMYSQLServer11projects/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:"" json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+}
