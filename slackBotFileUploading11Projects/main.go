8b5b5021b618f090c403115c1099011be012e440
Vladislav747 <zippo585@gmail.com>
2022-05-03 20:05:41 +0500

    Updated slackBotFileUploading11projects

--- slackBotFileUploading11Projects/main.go
+++ slackBotFileUploading11Projects/main.go
@@ -1,25 +1,10 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"github.com/shomali11/slacker"
 	"github.com/slack-go/slack"
-	"log"
 	"os"
-	"strconv"
 )
-
-//func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
-//	for event := range analyticsChannel {
-//		fmt.Println("Command Evenets")
-//		fmt.Println(event.Timestamp)
-//		fmt.Println(event.Command)
-//		fmt.Println(event.Parameters)
-//		fmt.Println(event.Event)
-//		fmt.Println()
-//	}
-//}
 
 func main() {
 	//API Slackbot
@@ -41,34 +26,5 @@ func main() {
 			return
 		}
 		fmt.Printf("Name: %s, Url:%s\n", file.Name, file.URL)
-	}
-
-	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
-
-	//Горутина обработки команд
-	go printCommandEvents(bot.CommandEvents())
-
-	bot.Command("My job is to...", &slacker.CommandDefinition{
-		Description: "you calcualtor",
-		Example:     "my yob is 2020",
-		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
-			year := request.Param("year")
-			//year of birth - yob
-			yob, err := strconv.Atoi(year)
-			if err != nil {
-				println("error")
-			}
-			age := 2021 - yob
-			r := fmt.Sprintf("age is %d", age)
-			response.Reply(r)
-		},
-	})
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	err := bot.Listen(ctx)
-	if err != nil {
-		log.Fatal(err)
 	}
 }
