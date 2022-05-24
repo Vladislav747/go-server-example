5bb1eed601dd4c312543a53858f39c5b0cba6c96
Vladislav747 <zippo585@gmail.com>
2022-05-25 00:28:10 +0500

    Added path resolver

--- pathHandler/main.go
+++ pathHandler/main.go
@@ -1,12 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"path"
+	"strings"
+)
 
 func main() {
 	pr := newPathResolver()
 	pr.Add("GET /hello", hello)
 	pr.Add("* /goodbye", goodbye)
-	http.ListenAndServe("8080", pr)
+	err := http.ListenAndServe(":8079", pr)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func newPathResolver() *pathResolver {
@@ -22,5 +30,34 @@ func (p *pathResolver) Add(path string, handler http.HandlerFunc) {
 }
 
 func (p *pathResolver) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	ni
+	check := req.Method + " " + req.URL.Path
+	for pattern, handlerFunc := range p.handlers {
+		if ok, err := path.Match(pattern, check); ok && err == nil {
+			handlerFunc(res, req)
+			return
+		} else if err != nil {
+			fmt.Fprint(res, err)
+		}
+	}
+
+	http.NotFound(res, req)
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+	name := query.Get("name")
+	if name == "" {
+		name = "Inigo Montoya"
+	}
+	fmt.Fprint(res, "Hello my name is ", name)
+}
+
+func goodbye(res http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path
+	parts := strings.Split(path, "/")
+	name := parts[0]
+	if name == "" {
+		name = "Inigo Montoya"
+	}
+	fmt.Fprint(res, "Goodbye ", name)
 }
